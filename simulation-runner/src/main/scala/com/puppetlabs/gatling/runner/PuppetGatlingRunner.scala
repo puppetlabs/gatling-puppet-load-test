c95c82edc9f49e0ef08a26c12bd26d001fd89220
Stephane Landelle <slandelle+github@excilys.com>
2013-06-05 22:19:58 +0200

    Since Gatling 1.5.1, exec can take a scenario parameter

--- simulation-runner/src/main/scala/com/puppetlabs/gatling/runner/PuppetGatlingRunner.scala
+++ simulation-runner/src/main/scala/com/puppetlabs/gatling/runner/PuppetGatlingRunner.scala
@@ -2,7 +2,6 @@ package com.puppetlabs.gatling.runner
 
 import com.excilys.ebi.gatling.app.Gatling
 import com.puppetlabs.gatling.config.PuppetGatlingConfig
-import com.puppetlabs.gatling.runner.ConfigDrivenSimulation
 
 /**
  * This object simply provides a `main` method that wraps
@@ -16,19 +15,8 @@ object PuppetGatlingRunner {
 
     val config = PuppetGatlingConfig.initialize()
 
-    // This is a pretty terrible hack, but because Gatling initializes
-    // all of its configuration settings from system properties and/or
-    // the gatling config file prior to loading any of our simulation
-    // classes, the only way for us to be able to override these
-    // settings with values from *our* config file is to do something
-    // before Gatling itself gets loaded.  The choice is basically
-    // limited to either dynamically writing out our own gatling.conf
-    // file or overriding the handful of system properties that we
-    // care about.  I chose the latter for now.
-
-
     // This sets the class for the simulation we want to run.
-    val simClass: String = classOf[ConfigDrivenSimulation].getName
+    val simClass = classOf[ConfigDrivenSimulation].getName
     System.setProperty("gatling.core.simulationClass", simClass)
 
     // This sets the "id" for the simulataion, which will be used

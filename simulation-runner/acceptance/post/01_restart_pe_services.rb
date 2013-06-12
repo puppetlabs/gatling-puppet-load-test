1b7d32c846a943fc65443d69f2bc1a3fdef8226d
Chris Price <chris@puppetlabs.com>
2013-06-11 22:51:22 -0600

    Update 01_restart_pe_services.rb

--- simulation-runner/acceptance/post/01_restart_pe_services.rb
+++ simulation-runner/acceptance/post/01_restart_pe_services.rb
@@ -1,6 +1,7 @@
 test_name = "Restart PE services"
 
-on master, "/etc/init.d/pe-httpd restart && /etc/init.d/pe-puppet-dashboard-workers restart"
+#on master, "/etc/init.d/pe-httpd restart && /etc/init.d/pe-puppet-dashboard-workers restart"
+on master, "/etc/init.d/pe-httpd restart && service pe-puppet-dashboard-workers restart"
 on master, "/etc/init.d/pe-mcollective restart && /etc/init.d/pe-activemq restart"
 on master, "/etc/init.d/pe-memcached restart"
 on master, "/etc/init.d/pe-puppet restart"

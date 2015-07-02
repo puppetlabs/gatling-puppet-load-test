995185b6761b3d366a3afb57247c1b04f7f14669
Nate Wolfe <nwolfe@puppetlabs.com>
2015-07-02 12:35:13 -0700

    (SERVER-687) Classify PE nodes
    This commit adds PE support for node classification via the Node
    Classifier and the Puppet Labs 'scooter' gem. The Gemfile.lock is now
    ignored because it will be constantly changing as we run FOSS and PE as
    there are now gems that will only be used during PE runs.

--- jenkins-integration/beaker/install/pe/10_install_pe.rb
+++ jenkins-integration/beaker/install/pe/10_install_pe.rb
@@ -1,3 +1,7 @@
 test_name 'Install PE'
 
+# Environment variables:
+#   (required) pe_dist_dir="http://neptune.puppetlabs.lan/4.0/ci-ready"
+#   (optional) pe_ver="4.0.0-rc5-161-g85ecc84"
+
 install_pe
--- jenkins-integration/scripts/pe_install.sh
+++ jenkins-integration/scripts/pe_install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -x
 
-export BEAKER_TESTSUITE="${BEAKER_TESTSUITE:-beaker/install/pe/}"
+# Requires version of PE (AIO-only) to install:
+#     pe_dist_dir="http://pe-releases.puppetlabs.lan/x.y.z"
+#     pe_dist_dir="http://neptune.puppetlabs.lan/4.0/ci-ready"
+#     (optional) pe_ver="4.0.0-rc5-161-g85ecc84"
+
+export BEAKER_TESTSUITE="
+beaker/install/pe/10_install_pe.rb,\
+beaker/install/shared/40_clone_test_catalogs.rb,\
+beaker/install/shared/50_install_modules.rb,\
+beaker/install/pe/60_classify_nodes.rb
+"
 export BEAKER_KEYFILE="~/.ssh/id_rsa-acceptance"
 export BEAKER_HELPER="beaker/helper.rb"
 
@@ -11,7 +21,9 @@ bundle exec beaker \
   --config $BEAKER_CONFIG \
   --type aio \
   --helper $BEAKER_HELPER \
+  --load-path lib \
   --tests $BEAKER_TESTSUITE \
   --keyfile $BEAKER_KEYFILE \
   --preserve-hosts onfail \
-  --debug 
+  --no-color \
+  --debug

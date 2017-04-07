94d3c2c3752ab4f96907bd8d55b04149118ad578
Jeremy Barlow <jeremy.barlow@puppet.com>
2017-04-07 08:37:26 -0700

    (PDB-3209) Configure EPEL repo during deps installation
    This commit configures the EPEL repo during dependency installation.
    This enables the step added in a previous step to install jq for the new
    PuppetDB test to work properly.

--- jenkins-integration/beaker/install/shared/install_deps.rb
+++ jenkins-integration/beaker/install/shared/install_deps.rb
@@ -1,3 +1,9 @@
+step "Configure epel" do
+  tmp_module_dir = master.tmpdir('configure_epel')
+  on(master, puppet('module', 'install', 'stahnma-epel', '--codedir', tmp_module_dir))
+  on(master, puppet('apply', '-e', "'include epel'"))
+end
+
 step "Install jq" do
   install_package master, 'jq'
 end

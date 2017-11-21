d3ae4cf53353d72ee812687284c966be87f0804d
samwoods1 <sam.woods@puppet.com>
2017-11-21 12:56:10 -0800

    (PE-3353) Allow pe_dir to be an internal network address for AWS install

--- setup/install/00_pre_install/40_ec2_fixes.rb
+++ setup/install/00_pre_install/40_ec2_fixes.rb
@@ -11,5 +11,18 @@ test_name 'Workaround various EC2 host config issues.' do
       cloud_config(host)
     end
   end
+
+  step 'Download Puppet installer and set pe_dir' do
+    # If the installer download is on the internal network, EC2 instance can't access it, so copy it locally.
+    if master['pe_dir'] =~ /puppetlabs\.net/
+      tmp_dir = Dir.mktmpdir
+      file = "#{master[:dist]}.tar.gz"
+      system("curl -o #{tmp_dir}/#{file} #{master[:pe_dir]}/#{file}")
+      hosts.each do |host|
+        host[:pe_dir] = tmp_dir
+      end
+    end
+  end
 end
+
 

cd2fe01e8dcfe6f137ae9db80373f23ab551f911
samwoods1 <sam.woods@puppet.com>
2017-12-14 12:45:11 -0800

    (maint) Fix auth.conf, always assume TKA Auth enabled

--- setup/install_gatling/30_post_install/30_configure_permissive_server_auth.rb
+++ setup/install_gatling/30_post_install/30_configure_permissive_server_auth.rb
@@ -1,13 +1,9 @@
 test_name 'Configure permissive auth on Puppet Server'
 
 step 'Configure permissive auth.conf on master' do
-  supports_tk_auth = ENV['PUPPET_SERVER_TK_AUTH'] == 'true'
 
-  if supports_tk_auth
-    Beaker::Log.notify "Server supports tk auth, configuring."
-
-    auth_conf = '/etc/puppetlabs/puppetserver/conf.d/auth.conf'
-    create_remote_file(master, auth_conf, <<-EOF)
+  auth_conf = '/etc/puppetlabs/puppetserver/conf.d/auth.conf'
+  create_remote_file(master, auth_conf, <<-EOF)
 authorization: {
     version: 1
     rules: [
@@ -22,18 +18,6 @@ authorization: {
         }
     ]
 }
-    EOF
-  else
-    Beaker::Log.notify "Server does not support tk auth, configuring legacy auth.conf"
-
-    auth_conf = '/etc/puppetlabs/puppet/auth.conf'
-    create_remote_file(master, auth_conf, <<-EOF)
-path /
-auth any
-allow *
-    EOF
-
-  end
-
+  EOF
 
 end

a02d9a0ca7be5bf27c4d406444362c81ca393888
Matthaus Owens <matthaus@puppet.com>
2018-02-02 15:52:20 -0800

    (SERVER-2108) Comment at the param description instead of deep in the grooooves.

--- jenkins-integration/jenkins-jobs/scenarios/puppetserver-infinite/JobDSL.groovy
+++ jenkins-integration/jenkins-jobs/scenarios/puppetserver-infinite/JobDSL.groovy
@@ -1,6 +1,5 @@
 job.parameters {
     stringParam('NUMBER_OF_HOURS', '2', 'Length of time to run the gatling simulation.')
     choiceParam('CATALOG_SIZE', ['MEDIUM', 'EMPTY'], 'Catalog to use in simulation.')
-    // NODE_COUNT is offered as a list because gatling will not handle values that don't divide 1800 evenly (multiples of 300 also seem fine, which is why 1200 and 1500 are also there).
-    choiceParam('NODE_COUNT', ['600', '100', '200', '300', '900', '1200', '1500', '1800'], 'Number of nodes to include in simulation.')
+    choiceParam('NODE_COUNT', ['600', '100', '200', '300', '900', '1200', '1500', '1800'], 'Number of nodes (these were selected as they produce an even node distribution) to include in simulation.')
 }

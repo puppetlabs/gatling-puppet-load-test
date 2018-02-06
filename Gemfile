3b95eb81b7f79d939372e89be63407331033f1bd
samwoods1 <sam.woods@puppet.com>
2018-02-05 16:18:26 -0800

    (QA-3352) Use beaker-benchmark to manage atop We needed to log atop results in a CSV format to make it so we can do some data analysis, and use beaker-benchmark to get averages and summarize the results. Also updated the code to store results to include important gatling and atop logs that are stored. Also fixed number of hosts in the ApplesToApples scenario since 750 actually means 600 because of the way that Gatling schedules things.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source ENV['GEM_SOURCE'] || 'https://artifactory.delivery.puppetlabs.net/artifactory/api/gems/rubygems/'
 
 gem 'beaker', :git => 'https://github.com/puppetlabs/beaker.git', :branch => 'fix/master/BKR-487_preserved_hosts_yml_fix'
+gem 'beaker-benchmark', '~>0.0'
 gem 'beaker-pe', '~>1.4'
 gem 'beaker-pe-large-environments', '~>0.2'
 gem 'scooter', '~>4.0'
--- tests/gatling_kickoff.rb
+++ tests/gatling_kickoff.rb
@@ -1,6 +1,25 @@
-step "Execute gatling scenario" do
-  on metric, "cd /root/gatling-puppet-load-test/simulation-runner/ && " +
-      "PUPPET_GATLING_MASTER_BASE_URL=https://#{master.hostname}:8140 " +
-      "PUPPET_GATLING_SIMULATION_CONFIG=config/scenarios/#{ENV['PUPPET_GATLING_SCENARIO']} " +
-      "PUPPET_GATLING_SIMULATION_ID=PerfTestLarge sbt run"
+require 'beaker-benchmark'
+
+test_name 'Kickoff Gatling' do
+
+  start_monitoring(master, 'apples to apples perf run', true, 30)
+
+  begin
+    step "Execute gatling scenario" do
+      on metric, "cd /root/gatling-puppet-load-test/simulation-runner/ && " +
+          "PUPPET_GATLING_MASTER_BASE_URL=https://#{master.hostname}:8140 " +
+          "PUPPET_GATLING_SIMULATION_CONFIG=config/scenarios/#{ENV['PUPPET_GATLING_SCENARIO']} " +
+          "PUPPET_GATLING_SIMULATION_ID=PerfTestLarge sbt run",
+         {:accept_all_exit_codes => true}
+    end
+
+  ensure
+    perf_result = stop_monitoring(master, '/opt/puppetlabs')
+    if perf_result
+      perf_result.log_summary
+      # Write summary results to log so it can be archived
+      perf_result.log_csv
+    end
+  end
+
 end

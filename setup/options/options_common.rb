0c7f25b63d181cddef5b7f294fb3d68968d75b6f
Eric Thompson <eric.thompson@puppetlabs.com>
2018-04-12 11:33:04 -0700

    (SLV-89) create beaker options common and foss files
    * we can use the common file for any setup
      * contains common options like sshe key, helpers, beaker options
    * the foss file calls the correct pre_suites, in order
      * later we can remove the numbering on the files

---
+++ setup/options/options_common.rb
@@ -0,0 +1,16 @@
+{
+  :ssh                         => {
+    :keys => ["id_rsa_acceptance", "#{ENV['HOME']}/.ssh/id_rsa-acceptance"],
+  },
+  :helper                      => [
+    'setup/helpers/classification_helper.rb',
+    'setup/helpers/ldap_helper.rb',
+    'setup/helpers/gatling_config_helper.rb',
+  ],
+  :xml                         => true,
+  :timesync                    => false,
+  :repo_proxy                  => true,
+  :add_el_extras               => false,
+  :forge_host                  => 'forge-aio01-petest.puppetlabs.com',
+  :'master-start-curl-retries' => 30,
+}

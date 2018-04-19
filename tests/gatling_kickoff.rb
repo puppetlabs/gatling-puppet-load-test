1944c595c3323f86e7a8c2f4e9a28ca5383db02c
bill.claytor <bill.claytor@puppet.com>
2018-04-19 11:15:18 -0700

    (SLV-100) implement reports-only run

--- tests/gatling_kickoff.rb
+++ tests/gatling_kickoff.rb
@@ -6,11 +6,24 @@ test_name 'Kickoff Gatling' do
 
   begin
     step "Execute gatling scenario" do
+
+      # Should gatling run reports only?
+      if ENV['PUPPET_GATLING_REPORTS_ONLY'] == "true"
+        reports_only = "true"
+        reports_target = ENV['PUPPET_GATLING_REPORTS_TARGET']
+      else
+        reports_only = "false"
+        reports_target = ""
+      end
+
       on metric, "cd /root/gatling-puppet-load-test/simulation-runner/ && " +
           "PUPPET_GATLING_MASTER_BASE_URL=https://#{master.hostname}:8140 " +
           "PUPPET_GATLING_SIMULATION_CONFIG=config/scenarios/#{ENV['PUPPET_GATLING_SCENARIO']} " +
+          "PUPPET_GATLING_REPORTS_ONLY=#{reports_only} " +
+          "PUPPET_GATLING_REPORTS_TARGET=/root/gatling-puppet-load-test/simulation-runner/results/#{reports_target} " +
           "PUPPET_GATLING_SIMULATION_ID=PerfTestLarge sbt run",
          {:accept_all_exit_codes => true}
+
     end
 
   ensure

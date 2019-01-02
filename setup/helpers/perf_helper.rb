97f47ee79294cf54bcf0b7c75fd708c80e9f2f5e
RandellP <randell@puppet.com>
2019-01-02 10:57:39 -0800

    (SLV-283) refactored a bit
    added get_classifier so we don't have to do it over and over again.

--- setup/helpers/perf_helper.rb
+++ setup/helpers/perf_helper.rb
@@ -373,27 +373,26 @@ EOS
     on(master, curl)
   end
 
-  def classify_nodes_via_nc
-    # Classify any agent with the word 'agent' in it's hostname.
-    def classify_nodes(classifier)
-      classifier.find_or_create_node_group_model(
-          'parent' => '00000000-0000-4000-8000-000000000000',
-          'name' => 'perf-agent-group',
-          'rule' => ['~', ['fact', 'clientcert'], '.*agent.*'],
-          'classes' => { ENV['PUPPET_SCALE_CLASS'] => nil } )
-    end
-
+  def get_classifier
     classifier = Scooter::HttpDispatchers::ConsoleDispatcher.new(dashboard, {:login => 'admin', :password => 'puppetlabs', :resolve_dns => true})
-
     # Updating classes can take a VERY long time, like the OPS deployment
     # which has ~80 environments each with hundreds of classes.
     # Set the connection timeout to 60 minutes to accomodate this.
     classifier.connection.options.timeout = 3600
     classifier.update_classes
-
-    classify_nodes(classifier)
+    return classifier
   end
 
+  def classify_nodes_via_nc
+    # Classify any agent with the word 'agent' in it's hostname.
+    # get classifier and set timeout
+    classifier = get_classifier
+    classifier.find_or_create_node_group_model(
+        'parent' => '00000000-0000-4000-8000-000000000000',
+        'name' => 'perf-agent-group',
+        'rule' => ['~', ['fact', 'clientcert'], '.*agent.*'],
+        'classes' => { ENV['PUPPET_SCALE_CLASS'] => nil } )
+  end
 
   def generate_sitepp()
     <<-sitepp
@@ -586,48 +585,28 @@ authorization: {
     end
   end
 
+
+
   def classify_metrics_node_via_nc
-    # Classify any agent with the word 'agent' in it's hostname and the metrics node
-    def classify_nodes(classifier)
-      metrics_cert_name = on(metric, puppet('config print certname')).stdout.chomp
-      classifier.find_or_create_node_group_model(
-          'parent' => '00000000-0000-4000-8000-000000000000',
-          'name' => 'perf-agent-group',
-          'rule' => [ 'or',
-                      ['~', ['fact', 'clientcert'], metrics_cert_name],
-                      ['~', ['fact', 'clientcert'], '.*agent.*'] ] )
-    end
-
-    classifier = Scooter::HttpDispatchers::ConsoleDispatcher.new(dashboard, {:login => 'admin', :password => 'puppetlabs', :resolve_dns => true})
-
-    # Updating classes can take a VERY long time, like the OPS deployment
-    # which has ~80 environments each with hundreds of classes.
-    # Set the connection timeout to 60 minutes to accomodate this.
-    classifier.connection.options.timeout = 3600
-    classifier.update_classes
-
-    classify_nodes(classifier)
+    metrics_cert_name = on(metric, puppet('config print certname')).stdout.chomp
+    # get classifier and set timeout
+    classifier = get_classifier
+    classifier.find_or_create_node_group_model(
+        'parent' => '00000000-0000-4000-8000-000000000000',
+        'name' => 'perf-agent-group',
+        'rule' => [ 'or',
+                    ['~', ['fact', 'clientcert'], metrics_cert_name],
+                    ['~', ['fact', 'clientcert'], '.*agent.*'] ] )
   end
 
   def classify_master_node_via_nc
-    def classify_nodes(classifier)
-      master_cert_name = on(master, puppet('config print certname')).stdout.chomp
-      classifier.find_or_create_node_group_model(
-          'parent' => '00000000-0000-4000-8000-000000000000',
-          'name' => 'master-group',
-          'rule' => [ '~', ['fact', 'clientcert'], master_cert_name],
-          'classes' => { 'role::puppet_master' => nil } )
-    end
-
-    classifier = Scooter::HttpDispatchers::ConsoleDispatcher.new(dashboard, {:login => 'admin', :password => 'puppetlabs', :resolve_dns => true})
-
-    # Updating classes can take a VERY long time, like the OPS deployment
-    # which has ~80 environments each with hundreds of classes.
-    # Set the connection timeout to 60 minutes to accomodate this.
-    classifier.connection.options.timeout = 3600
-    classifier.update_classes
-
-    classify_nodes(classifier)
+    master_cert_name = on(master, puppet('config print certname')).stdout.chomp
+    # get classifier and set timeout
+    classifier = get_classifier
+    classifier.find_or_create_node_group_model(
+        'parent' => '00000000-0000-4000-8000-000000000000',
+        'name' => 'master-group',
+        'rule' => [ '~', ['fact', 'clientcert'], master_cert_name],
+        'classes' => { 'role::puppet_master' => nil } )
   end
-
 end

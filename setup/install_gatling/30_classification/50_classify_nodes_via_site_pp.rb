c668ad0a965124cf1110104eebb36be52504cfe5
Justin Stoller <justin.stoller@gmail.com>
2019-03-15 15:24:34 -0700

    more changes

--- setup/install_gatling/30_classification/50_classify_nodes_via_site_pp.rb
+++ setup/install_gatling/30_classification/50_classify_nodes_via_site_pp.rb
@@ -2,3 +2,4 @@ test_name "Classify Puppet agents on master"
 skip_test 'Installing PE, not FOSS' unless ENV['BEAKER_INSTALL_TYPE'] == 'foss'
 
 classify_foss_nodes(master)
+setup_puppet_metrics_collector_for_foss

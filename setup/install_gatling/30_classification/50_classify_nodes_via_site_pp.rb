78ab1193b64621d44cff2717c5b4e950032fd0cc
John Duarte <john.duarte@puppet.com>
2019-05-29 15:39:03 -0700

    rm classifier data sources from foss hiera configs
    This commit updates the classification setup for foss deployments
    to ensure that any classifier data sources are removed from hiera.yaml
    config files since these do not exist on a foss installation.

--- setup/install_gatling/30_classification/50_classify_nodes_via_site_pp.rb
+++ setup/install_gatling/30_classification/50_classify_nodes_via_site_pp.rb
@@ -1,5 +1,23 @@
-test_name "Classify Puppet agents on master"
-skip_test 'Installing PE, not FOSS' unless ENV['BEAKER_INSTALL_TYPE'] == 'foss'
+test_name "Classify Puppet agents on master" do
+  skip_test "Only runs on FOSS" unless ENV["BEAKER_INSTALL_TYPE"] == "foss"
 
-classify_foss_nodes(master)
-setup_puppet_metrics_collector_for_foss
+  # Remove classifier data sources from the hiera configs on foss
+  hiera_configs = []
+
+  hc_global = puppet_config(master, "hiera_config")
+  hiera_configs << hc_global if master.file_exist?(hc_global)
+
+  env_path = puppet_config(master, "environmentpath")
+  env = puppet_config(master, "environment")
+  hc_env = [env_path, env, "hiera.yaml"].join("/")
+  hiera_configs << hc_env if master.file_exist?(hc_env)
+
+  hiera_configs.each do |hiera_config|
+    config = YAML.load(on(master, "cat #{hiera_config}").stdout)
+    config["hierarchy"].reject! { |backend| backend["name"] =~ /Classifier/i }
+    create_remote_file(master, hiera_config, config.to_yaml)
+  end
+
+  classify_foss_nodes(master)
+  setup_puppet_metrics_collector_for_foss
+end

5c52cd14e5500df802ea6f16abff9421860415af
bill.claytor <bill.claytor@puppet.com>
2019-07-03 08:00:11 -0700

    (SLV-365) Updated provision_pe_xl_nodes script and templates - create nodes.yaml using heredoc - move nodes.yaml and params.json generation into separate methods - remove nodes.yaml templates - add ha parameter to params.json templates - update params_no_ha.json template to remove replica roles

--- util/abs/provision_pe_xl_nodes.rb
+++ util/abs/provision_pe_xl_nodes.rb
@@ -27,12 +27,31 @@ ROLES = ROLES_CORE + ROLES_HA
 
 # TODO: move to spec when test cases are implemented
 # for now this allows testing of the create_pe_xl_bolt_files method without provisioning
-TEST_HOSTS = [{ role: "puppet_db", hostname: "ip-10-227-3-22.amz-dev.puppet.net" },
-              { role: "compiler_b", hostname: "ip-10-227-1-195.amz-dev.puppet.net" },
-              { role: "puppet_db_replica", hostname: "ip-10-227-3-158.amz-dev.puppet.net" },
-              { role: "master", hostname: "ip-10-227-3-127.amz-dev.puppet.net" },
-              { role: "compiler_a", hostname: "ip-10-227-3-242.amz-dev.puppet.net" },
-              { role: "master_replica", hostname: "ip-10-227-1-82.amz-dev.puppet.net" }].freeze
+TEST_HOSTS_HA = [{ role: "puppet_db", hostname: "ip-10-227-3-22.amz-dev.puppet.net" },
+                 { role: "compiler_b", hostname: "ip-10-227-1-195.amz-dev.puppet.net" },
+                 { role: "puppet_db_replica", hostname: "ip-10-227-3-158.amz-dev.puppet.net" },
+                 { role: "master", hostname: "ip-10-227-3-127.amz-dev.puppet.net" },
+                 { role: "compiler_a", hostname: "ip-10-227-3-242.amz-dev.puppet.net" },
+                 { role: "master_replica", hostname: "ip-10-227-1-82.amz-dev.puppet.net" }].freeze
+
+TEST_HOSTS_NO_HA = [{ role: "puppet_db", hostname: "ip-10-227-3-22.amz-dev.puppet.net" },
+                    { role: "compiler_b", hostname: "ip-10-227-1-195.amz-dev.puppet.net" },
+                    { role: "master", hostname: "ip-10-227-3-127.amz-dev.puppet.net" },
+                    { role: "compiler_a", hostname: "ip-10-227-3-242.amz-dev.puppet.net" }].freeze
+
+NODES_YAML_START = <<~NODES
+  ---
+  groups:
+    - name: pe_xl_nodes
+      config:
+        transport: ssh
+        ssh:
+          host-key-check: false
+          user: centos
+          run-as: root
+          tty: true
+      nodes:
+NODES
 
 # Creates the Bolt inventory file (nodes.yaml) and
 # parameters file (params.json) for the specified hosts
@@ -41,7 +60,8 @@ TEST_HOSTS = [{ role: "puppet_db", hostname: "ip-10-227-3-22.amz-dev.puppet.net"
 #
 # @author Bill Claytor
 #
-# @param [Array<Hash>] The hosts
+# @param [Array<Hash>] hosts The provisioned hosts
+# @param [String] output_dir The directory where the file should be written
 #
 # @example
 #   hosts = provision_hosts_for_roles(roles)
@@ -50,6 +70,45 @@ TEST_HOSTS = [{ role: "puppet_db", hostname: "ip-10-227-3-22.amz-dev.puppet.net"
 # TODO: move to abs_helper or elsewhere?
 # TODO: spec test(s)
 def create_pe_xl_bolt_files(hosts, output_dir)
+  create_nodes_yaml(hosts, output_dir)
+  create_params_json(hosts, output_dir)
+end
+
+# Creates the Bolt inventory file (nodes.yaml) for the specified hosts
+#
+# Note: designed to use the output of provision_hosts_for_roles
+#
+# @author Bill Claytor
+#
+# @param [Array<Hash>] hosts The provisioned hosts
+# @param [String] output_dir The directory where the file should be written
+#
+# @example
+#   hosts = provision_hosts_for_roles(roles)
+#   create_nodes_yaml(hosts, output_dir)
+def create_nodes_yaml(hosts, output_dir)
+  File.open("#{output_dir}/nodes.yaml", "w+") do |f|
+    f.puts NODES_YAML_START
+    hosts.each do |host|
+      hostname = host[:hostname]
+      f.puts "      - #{hostname}"
+    end
+  end
+end
+
+# Creates the Bolt parameters file (params.json) for the specified hosts
+#
+# Note: designed to use the output of provision_hosts_for_roles
+#
+# @author Bill Claytor
+#
+# @param [Array<Hash>] hosts The provisioned hosts
+# @param [String] output_dir The directory where the file should be written
+#
+# @example
+#   hosts = provision_hosts_for_roles(roles)
+#   create_params_json(hosts, output_dir)
+def create_params_json(hosts, output_dir)
   # HA?
   master_replica = hosts.detect { |m| m[:role] == "master_replica" }
   suffix = if master_replica
@@ -58,8 +117,6 @@ def create_pe_xl_bolt_files(hosts, output_dir)
              "no_ha"
            end
 
-  # TODO: better alternative to having separate files for ha / no ha?
-  nodes_yaml = File.read("#{TEMPLATE_DIR}/nodes_#{suffix}.yaml")
   params_json = File.read("#{TEMPLATE_DIR}/params_#{suffix}.json")
 
   # replace parameters for each host
@@ -75,15 +132,16 @@ def create_pe_xl_bolt_files(hosts, output_dir)
     puts
 
     # replace parameters
-    nodes_yaml = nodes_yaml.gsub(param, hostname)
     params_json = params_json.gsub(param, hostname)
   end
 
-  File.write("#{output_dir}/nodes.yaml", nodes_yaml)
   File.write("#{output_dir}/params.json", params_json)
 end
 
 abs_id = ARGV[0] || ABS_ID
 output_dir = ARGV[1] || "./"
+
+# TODO: update provision_hosts_for_roles to generate last_abs_resource_hosts.log
+# and generate Beaker hosts file
 hosts = provision_hosts_for_roles(ROLES, abs_id, ABS_SIZE, ABS_VOLUME_SIZE)
 create_pe_xl_bolt_files(hosts, output_dir)

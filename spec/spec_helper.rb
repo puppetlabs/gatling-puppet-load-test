9508aae1e216b37b9e1c89d4611c6938549daff0
bill.claytor <bill.claytor@puppet.com>
2019-07-22 09:30:30 -0700

    (SLV-526) Update spec_helper to remove unused example code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,17 +15,6 @@ Dir["./setup/helpers/*.rb"].each { |file| require file }
 # test helpers
 Dir["./tests/helpers/*.rb"].each { |file| require file }
 
-RSpec.configure do |c|
-end
-
-RSpec.shared_context "case_info_lets" do
-  let(:something) { "some value" }
-end
-
-def do_something_helpful(value)
-  puts "Do something with #{value}"
-end
-
 SimpleCov.at_exit do
   SimpleCov.result.format!
   SimpleCov.minimum_coverage 40

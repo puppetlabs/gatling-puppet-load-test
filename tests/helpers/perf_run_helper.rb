d3a652f45ad143fd6f5e6d09d7aecaad05d555f0
John Duarte <john.duarte@puppet.com>
2019-09-27 08:02:05 -0700

    (SLV-574) Add privatebindir to master if not avail
    This commit updates the `get_perf_result` method in the
    `perf_run_helper.rb` file.  It looks up the bindir of the installed
    puppet on the master if the `privatebindir` value has not been set.
    
    Prior to this commit, the call to `ruby /root/collect_metrics_files.rb`
    on the master would fail on a pe_xl deployed environment because beaker
    was not used to install puppet.  Without installing puppet, beaker does
    not assign a value to `privatebindir` on the host.  This results in ruby
    not being available on the PATH, so calling ruby fails.

--- tests/helpers/perf_run_helper.rb
+++ tests/helpers/perf_run_helper.rb
@@ -845,6 +845,13 @@ module PerfRunHelper
     scp_to(master, "util/metrics/collect_metrics_files.rb", "/root/collect_metrics_files.rb")
     start_epoch = File.read("#{@archive_root}/start_epoch")
     end_epoch = File.read("#{@archive_root}/end_epoch")
+
+    # privatebindir is not available if beaker did not install puppet.
+    # So, we introspect it from the host based on the installed puppet.
+    unless master["privatebindir"]
+      res = on(master, "readlink -f $(which puppet)")
+      master["privatebindir"] = File.dirname(res.stdout.chomp)
+    end
     cmf_output = on(master,
                     "env PATH=\"#{master['privatebindir']}:${PATH}\" \
                     ruby /root/collect_metrics_files.rb --start_epoch #{start_epoch} --end_epoch #{end_epoch}")

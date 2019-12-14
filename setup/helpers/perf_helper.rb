84ac94f770c29b7df18ae30a125e9415c3281242
John Duarte <john.duarte@puppet.com>
2019-12-13 18:34:32 -0800

    (SLV-733) Fix perf-agent-group classes
    This commit replaces the `nil` value with an empty hash value for the
    interpolated PUPPET_SCALE_CLASS applied to the `perf-agent-group`.
    Prior to this commit the nil value unset the class assignment causing
    the desired class not to be applied when installing on top of a pe_xl
    deployment.

--- setup/helpers/perf_helper.rb
+++ setup/helpers/perf_helper.rb
@@ -395,7 +395,7 @@ module PerfHelper
       "parent"  => "00000000-0000-4000-8000-000000000000",
       "name"    => "perf-agent-group",
       "rule"    => ["~", %w[fact clientcert], ".*agent.*"],
-      "classes" => { ENV["PUPPET_SCALE_CLASS"] => nil }
+      "classes" => { ENV["PUPPET_SCALE_CLASS"] => {} }
     )
   end
 

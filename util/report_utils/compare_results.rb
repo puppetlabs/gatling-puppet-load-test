eaaaa5363e4f7c16e00f5be535611ae482847754
bill.claytor <bill.claytor@puppet.com>
2019-12-18 08:27:24 -0800

    (SLV-661) Update gatling2csv and compare_results to add min column

--- util/report_utils/compare_results.rb
+++ util/report_utils/compare_results.rb
@@ -19,16 +19,16 @@ def write_csv(output_path, result_a, result_b)
 
   # TODO: use release names
   CSV.open(output_path.to_s, "wb") do |csv|
-    csv << ["", "$RELEASE_A_NAME ($RELEASE_A_NUMBER)", "", "",
-            "$RELEASE_B_NAME ($RELEASE_B_NUMBER)", "", ""]
-    csv << ["Duration", "max ms", "mean ms", "std dev", "max ms", "mean ms",
+    csv << ["", "$RELEASE_A_NAME ($RELEASE_A_NUMBER)", "", "", "",
+            "$RELEASE_B_NAME ($RELEASE_B_NUMBER)", "", "", "", ""]
+    csv << ["Duration (ms)", "min", "max", "mean", "std dev", "min", "max", "mean",
             "std dev", "% (mean) diff"]
 
     row_labels.each_with_index do |item, index|
       res_index = index + 1
-      csv << [item, result_a[res_index][1], result_a[res_index][2], result_a[res_index][3],
-              result_b[res_index][1], result_b[res_index][2], result_b[res_index][3],
-              percent_diff_string(result_a[res_index][2], result_b[res_index][2])]
+      csv << [item, result_a[res_index][1], result_a[res_index][2], result_a[res_index][3], result_a[res_index][4],
+              result_b[res_index][1], result_b[res_index][2], result_b[res_index][3], result_b[res_index][4],
+              percent_diff_string(result_a[res_index][3], result_b[res_index][3])]
     end
   end
 end

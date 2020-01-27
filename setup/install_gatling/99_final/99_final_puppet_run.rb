ce2207a8ff64c0f12f89b0fd2a6a77dcc672f9c6
John Duarte <john.duarte@puppet.com>
2020-01-27 10:49:46 -0800

    (SLV-738) Ensure puppet agent disabled on infra
    This commit adds a pre-suite step after the final puppet run to ensure
    that the puppet agent service is disabled on all hosts prior to any
    gatling tests.

--- setup/install_gatling/99_final/99_final_puppet_run.rb
+++ setup/install_gatling/99_final/99_final_puppet_run.rb
@@ -4,4 +4,12 @@ test_name "final puppet run" do
   step "Run puppet until all changes are applied" do
     run_agent_until_no_change(hosts)
   end
+  step "Ensure puppet agent disabled" do
+    # If deployed via install_pe this is part of the install.
+    #   Ref: https://github.com/puppetlabs/beaker-pe/blob/master/lib/beaker-pe/install/pe_utils.rb
+    # This is added to ensure that the puppet agent service is
+    # disabled prior to a gatling run regardless of how the hosts
+    # were deployed.
+    stop_agent_on(hosts)
+  end
 end

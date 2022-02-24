9ec156c45d5604449cc416a59d340795fefbb965
Samuel <samuel@puppet.com>
2022-02-24 08:16:34 -0600

    Update initialize_ruby_env.sh
    the minimum version in jenkins is now 2.5.9
    DIO-2951

--- jenkins-integration/jenkins-jobs/common/scripts/job-steps/initialize_ruby_env.sh
+++ jenkins-integration/jenkins-jobs/common/scripts/job-steps/initialize_ruby_env.sh
@@ -4,7 +4,7 @@ echo ""
 echo "Setting Up Ruby Environment"
 echo ""
 source /usr/local/rvm/scripts/rvm
-rvm use 2.4.1 || exit 1
+rvm use 2.5.9 || exit 1
 rvm list
 export GEM_SOURCE=https://artifactory.delivery.puppetlabs.net/artifactory/api/gems/rubygems/
 echo -e "\n\n\n"
